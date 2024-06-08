327573c8b9a918d58009a7d03294fb0f1e65aee6
Javier Lopez <javierlopezguzman00@gmail.com>
2024-06-07 21:48:27 -0600

    static frontend

--- Backend/index.js
+++ Backend/index.js
@@ -4,15 +4,12 @@ import 'dotenv/config'
 
 const app = express()
 app.use(express.json())
-const allowedOrigins = [`http://${process.env.IP}:5173`, 'https://metaphoto-frontend.s3.amazonaws.com'];
 
+app.use(express.static('./../Frontend/dist'));
 
 // Website allow to connect and request methods/headers allow
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
+  res.setHeader('Access-Control-Allow-Origin', `http://${process.env.IP}:5173`);
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Access-Control-Allow-Headers', '*');
   next();
