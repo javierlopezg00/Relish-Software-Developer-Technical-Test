37e7631038620e89e03565e3d68541dcb145a164
Javier Lopez <javierlopezguzman00@gmail.com>
2024-06-07 22:54:50 -0600

    produ branch

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   // External API request and state setting 
   const getSongs = async (filters) => {
     try {
-      const response = await axios.get("http://localhost:80/externalapi/photos", {
+      const response = await axios.get("http://54.208.54.254/externalapi/photos", {
         params: {
           ...filters
         }
